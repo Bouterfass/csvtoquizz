92765910ec78539355356e90f42986344863dd44
bouterfass <cefyou28@gmail.com>
2024-10-10 04:48:46 +0200

    next

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,41 +1,46 @@
 import React, { useEffect, useState } from "react";
-import { Input } from '@headlessui/react'
+import { Input } from "@headlessui/react";
 
 interface Question {
-    column: string;
-    column_two: string;
+  column: string;
+  column_two: string;
 }
 
 interface QuestionProps {
   question: Question;
   onCorrectAnswer: (value: string) => void;
-  key: number;
+  id: number;
 }
 
-
-
-const Question = ({ question, onCorrectAnswer, key }: QuestionProps) => {  
-
- 
-  
-
+const Question = ({ question, onCorrectAnswer, id }: QuestionProps) => {
   const handleChange = (event: any) => {
     const value = event.target.value;
     if (value === question.column) {
       event.target.value = "";
       onCorrectAnswer("good");
     }
+  };
 
-  }
+  const handleKey = (event: any) => {
+    if (event.keyCode === 13) {
+      if (event.target.value === question.column) onCorrectAnswer("good");
+      else onCorrectAnswer("bad");
+    }
+  };
 
-  return <>
-    <span>{question.column_two}</span>
-    <Input 
-    className="w-80 border-solid border-2 border-midnight"
-    name="answer" 
-    type="text"
-    onChange={handleChange} />
-  </>;
+  return (
+    <>
+      <span>{question.column_two}</span>
+      <Input
+        autoFocus
+        className="w-80 border-solid border-2 border-midnight"
+        name="answer"
+        type="text"
+        onChange={handleChange}
+        onKeyDown={handleKey}
+      />
+    </>
+  );
 };
 
 export default Question;
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Question from "../components/Question";
 import { Button } from "@headlessui/react";
 
-
 const Test = () => {
   let location = useLocation();
+  let btnRef = useRef<HTMLButtonElement>(null);
   const navigate = useNavigate();
   const [index, setIndex] = useState<number>(0);
   const [showAns, setShowAns] = useState<Boolean>(false);
@@ -13,33 +13,45 @@ const Test = () => {
   let data = location.state.key;
   let dataLength: number = data.length;
 
-  const nextQuestion = (value: string) => {
 
+
+
+  const nextQuestion = (value: string) => {
     if (index === dataLength - 1) {
-        navigate('/save');
+      navigate("/save");
     } else {
-      if (value === 'good') {
-        setIndex(index => index + 1);
-      } else {
-        if (showAns) {
-          setIndex(index => index + 1);
-          setShowAns(false);
-        } else {
-          setShowAns(true)
-        }
-      }
+      if (value === "good") setIndex((index) => index + 1);
+      else setShowAns(true);
     }
-  }
+  };
+
+  const handleAnswer = () => {
+    if (showAns) {
+      setIndex((index) => index + 1);
+      setShowAns(false);
+    } else {
+      setShowAns(true);
+    }
+  };
 
   return (
     <div className="flex flex-col">
       <h1>test page</h1>
-      <span>{index + 1}/{dataLength}</span>
-      <Question question={data[index]} onCorrectAnswer={nextQuestion} key={index} />
-      {showAns && <span>{data[index]['column']}</span>}
-      <Button 
-      className="w-32 rounded bg-midnight py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700"
-      onClick={() => nextQuestion("bad")}
+      <span>
+        {index + 1}/{dataLength}
+      </span>
+      {!showAns && (
+        <Question
+          question={data[index]}
+          onCorrectAnswer={nextQuestion}
+          id={index}
+        />
+      )}
+      {showAns && <span>{data[index]["column"]}</span>}
+      <Button
+        ref={btnRef}
+        className="w-32 rounded bg-midnight py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700"
+        onClick={handleAnswer}
       >
         {!showAns ? <span>show answer</span> : <span>next</span>}
       </Button>
